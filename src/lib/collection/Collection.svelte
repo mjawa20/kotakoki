6f25c65bac4be4f4bd4748fb1e5193ff84ea5643
m.jawa <m.jawahiruzzaman@gmail.com>
2022-05-24 15:40:33 +0700

    skeleton

--- src/lib/collection/Collection.svelte
+++ src/lib/collection/Collection.svelte
@@ -1,15 +1,21 @@
 <script>
+	import CollectionsListSkel from '../skeleton/CollectionsListSkel.svelte';
+
 	export let collection;
 	import CollectionItem from './CollectionItem.svelte';
 </script>
 
 <div class="mb-14">
 	<h1 class="font-bold text-2xl text-amber-900 mb-5">Collection list</h1>
-	<div class="grid md:grid-cols-4 grid-cols-2 gap-6">
-		{#each $collection as collection}
-			<CollectionItem {...collection} />
-		{/each}
-	</div>
+	{#if $collection.length}
+		<div class="grid md:grid-cols-4 grid-cols-2 gap-6">
+			{#each $collection as collection}
+				<CollectionItem {...collection} />
+			{/each}
+		</div>
+	{:else}
+		<CollectionsListSkel />
+	{/if}
 </div>
 <div class="w-full md:w-1/2 ">
 	<h1 class="font-bold text-2xl text-amber-900 mb-5">Collection list</h1>
