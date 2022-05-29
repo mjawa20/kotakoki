eb2c201c74439eb1d9d2d24c313e5e0bd1421f36
Gin <akmalulginan@gmail.com>
2022-05-29 22:58:04 +0700

    fix style sidebar

--- src/lib/sidebar/Sidebar.svelte
+++ src/lib/sidebar/Sidebar.svelte
@@ -83,12 +83,12 @@
 			<!-- Navigation -->
 			<ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
 				{#each items as item}
-					<li class="inline-flex">
+					<li class="inline-flex group hover:bg-red-500 hover:text-white p-3">
 						<a
 							href={item.link}
-							class="text-gray-700 hover:text-gray-500 text-sm block mb-4 no-underline font-semibold"
+							class=" text-sm block no-underline font-semibold"
 						>
-							<i class="fas fa-{item.icon} mr-2 text-gray-300 text-base w-4" />
+							<i class="fa-fw fas fa-{item.icon} mr-2 text-gray-300 group-hover:text-white text-base" />
 							{item.title}
 						</a>
 					</li>
