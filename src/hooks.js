68354bd7f869b6e762adfca1938e19adc0ddab35
m.jawa <m.jawahiruzzaman@gmail.com>
2022-06-06 13:40:48 +0700

    fix conflict

--- src/hooks.js
+++ src/hooks.js
@@ -7,7 +7,7 @@ const sessionHandler = handleSession({
 
 const routes = {
   private: [
-    '/account',
+    '/profile',
     '/user'
   ],
   auth: [
@@ -16,8 +16,7 @@ const routes = {
   ],
   public: [
     '/',
-    '/cart',
-    '/collections/1/product/1f3c70072c5'
+    '/cart'
   ]
 }
 
@@ -26,28 +25,6 @@ export const handle = sequence(sessionHandler, async ({ resolve, event }) => {
   if (event.locals.session.data?.authenticated && routes.auth.includes(event.url.pathname)) {
     return Response.redirect(event.url.origin + "/", 303);
   }
-<<<<<<< HEAD
-  
-  event.locals.authenticated = false;
-
-  if (routes.private.includes(event.url.pathname) || !routes.auth.includes(event.url.pathname)) {
-    if (!cookies.session_id && !routes.public.includes(event.url.pathname)) {
-      return Response.redirect(event.url.origin + "/", 303);
-    }
-
-    let userSession;
-    if (cookies.session_id) {
-      userSession = await db.models.user.findOne({ where: { sessionId: cookies.session_id } })
-    }
-
-    if (userSession) {
-      event.locals.authenticated = true;
-      event.locals.name = userSession.name;
-      event.locals.email = userSession.email;
-    } 
-  }
-=======
->>>>>>> f1a774272e6939885eae00d19469df7006f8ae40
 
   const response = await resolve(event);
   return response;
