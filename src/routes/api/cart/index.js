4666ddab2a13863767f545b41fd95bfb30fe7c75
m.jawa <m.jawahiruzzaman@gmail.com>
2022-06-07 21:30:48 +0700

    condition add to cart when same product added, use update by id set qty++
    Co-authored-by: akmalulginan <akmalulginan@users.noreply.github.com>

--- src/routes/api/cart/index.js
+++ src/routes/api/cart/index.js
@@ -2,30 +2,36 @@ import db from '../../../../db';
 import { filterBuilder, responseBuilder } from '../_api';
 
 export async function get({ url }) {
-    try {
-        const categories = await db.models.cart.findAndCountAll({ include: ['product'], ...filterBuilder(url) });
-        return responseBuilder(200, 'success', categories);
-    } catch (error) {
-        return responseBuilder(400, error);
-    }
+	try {
+		const categories = await db.models.cart.findAndCountAll({ include: ['product'], ...filterBuilder(url) });
+		return responseBuilder(200, 'success', categories);
+	} catch (error) {
+		return responseBuilder(400, error);
+	}
 }
 
 export async function post({ request }) {
-    try {
-        const cart = await request.json();
-        await db.models.cart.create(cart);
-        return responseBuilder(200, 'cart has been created', cart);
-    } catch (error) {
-        return responseBuilder(400, error);
-    }
+	try {
+		const cart = await request.json();
+		const existing = await db.models.cart.findOne({ where: { productId: cart.productId, userId: cart.userId } });
+		if (existing) {
+			existing.quantity++
+			await existing.save()
+			return responseBuilder(200, 'cart has been created', cart);
+		}
+		await db.models.cart.create(cart);
+		return responseBuilder(200, 'cart has been created', cart);
+	} catch (error) {
+		return responseBuilder(400, error);
+	}
 }
 
 export async function put({ request }) {
-    try {
-        const cart = await request.json();
-        await db.models.cart.update(cart, { where: { id: cart.id } });
-        return responseBuilder(200, 'cart has been updated', cart);
-    } catch (error) {
-        return responseBuilder(400, error);
-    }
+	try {
+		const cart = await request.json();
+		await db.models.cart.update(cart, { where: { id: cart.id } });
+		return responseBuilder(200, 'cart has been updated', cart);
+	} catch (error) {
+		return responseBuilder(400, error);
+	}
 }
